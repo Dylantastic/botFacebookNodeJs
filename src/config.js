270d67c6f96394e29113229f43a12c1b4e712b28
Ernest <dead.budy0811@gmail.com>
2024-12-10 00:59:34 +0800

    latest update

---
+++ src/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    verifyToken: "justverifytoken123456"
+};
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,13 @@
 import express from "express";
 import homepageController from "../controllers/homepageController";
+import chatBotController from "../controllers/chatBotController";
 let router = express.Router();
 
 let initWebroutes = (app) => {
 
     router.get("/", homepageController.getHomePage);
+    router.get("/webhook", chatBotController.getWebhook);
+    router.post("/webhook", chatBotController.postWebhook);
   return app.use("/", router);
 };
 
