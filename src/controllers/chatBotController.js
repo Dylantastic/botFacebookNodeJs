a0efeb7684510ee805744a41cc4da599d2fbf68d
Ernest <dead.budy0811@gmail.com>
2024-12-10 13:35:52 +0800

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -37,6 +37,48 @@ let getWebhook = (req, res) => {
         }
     }
 };
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
+
+// Parse the request body from the POST
+let body = req.body;
+
+// Check the webhook event is from a Page subscription
+if (body.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+        // Gets the body of the webhook event
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        console.log('Sender PSID: ' + sender_psid);
+
+    });
+
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+} else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+}
 module.exports = {
     postWebhook: postWebhook,
     getWebhook: getWebhook
