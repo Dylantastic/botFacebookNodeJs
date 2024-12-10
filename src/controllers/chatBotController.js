af7c56df697d90f01f433f287207af174b4f6e6f
Ernest <dead.budy0811@gmail.com>
2024-12-10 13:52:23 +0800

    update1

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,18 +1,31 @@
 require("dotenv").config();
 let postWebhook = (req, res) => {
+    // Parse the request body from the POST
     let body = req.body;
 
-        if (body.object === "page") {
-           body.entry.forEach(function (entry) {
-               let webhook_event = entry.messaging[0];
-               console.log(webhook_event);
-           });
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
 
-           res.status(200).send('EVENT_RECEIVE');
-        } else {
-            // Return a '404 Not Found' if event is not from a page subscription
-            res.sendStatus(404);
-        }
+        // Iterate over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+
+            // Gets the body of the webhook event
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
+
+        });
+
+        // Return a '200 OK' response to all events
+        res.status(200).send('EVENT_RECEIVED');
+
+    } else {
+        // Return a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
 
 };
 const config = require("../config.js"); // Adjust path as necessary
@@ -51,33 +64,6 @@ function handlePostback(sender_psid, received_postback) {
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
 
-}
-
-// Parse the request body from the POST
-let body = req.body;
-
-// Check the webhook event is from a Page subscription
-if (body.object === 'page') {
-
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-        // Gets the body of the webhook event
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-
-        // Get the sender PSID
-        let sender_psid = webhook_event.sender.id;
-        console.log('Sender PSID: ' + sender_psid);
-
-    });
-
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-
-} else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
 }
 module.exports = {
     postWebhook: postWebhook,
